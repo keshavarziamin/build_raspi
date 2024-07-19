0753e35d96ed357c85849139fbd838256a401176
keshavarzi.amin <keshavarzi.amin@gmail.com>
2024-07-19 12:34:08 +0330

    add install_docker.sh

---
+++ install_docker.sh
@@ -0,0 +1,20 @@
+sudo apt-get update -y
+sudo apt-get intall ca-certificate curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+suod chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# To install the latest version, run:
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+#test
+docker --version
+docker login
+sudo docker run hello-worldgit 
